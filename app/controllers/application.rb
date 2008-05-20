06b5dafa002417c3976c6666bed63dbdc211bf65
Scott Chacon <schacon@gmail.com>
2008-05-20 07:37:51 -0700

    initial groundcontrol rails deal

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '1d1d3d8e4db68349a2271d42d3e72f82'
+end
