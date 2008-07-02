8575854a8dff9e8807cd22caa4f0bba19e0c97ec
Scott Chacon <schacon@gmail.com>
2008-07-02 11:42:47 -0700

    better visualization of past performance runs

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -4,4 +4,20 @@ class Exercise < ActiveRecord::Base
   
   named_scope :recent, :order => 'created_at DESC', :limit => 10
   
+  def sample_size
+    samples.size
+  end
+  
+  def pass_fraction
+    passes = samples.select { |s| s.passed }.size
+    size = sample_size
+    "#{passes}/#{size}"
+  end
+  
+  def pass_percent
+    passes = samples.select { |s| s.passed }.size
+    size = sample_size
+    ((passes.to_f / size.to_f) * 100.0).round if size > 0
+  end
+  
 end
