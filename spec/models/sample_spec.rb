b493b8c03a5f3c2be4076c5003be30a119b09820
Gregory Tomei <gregtomei@gmail.com>
2008-07-09 15:07:46 -0700

    adding a simple test case for Sample. and clearing out the fixture file to add annotations and data.

--- spec/models/sample_spec.rb
+++ spec/models/sample_spec.rb
@@ -1,11 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe Sample do
+describe "Sample create" do
   before(:each) do
     @sample = Sample.new
   end
 
   it "should be valid" do
     @sample.should be_valid
+  end
+  
+  it "should be savable" do
+    @sample.save       .should equal(true)
+    @sample.new_record?.should equal(false)
   end
 end
