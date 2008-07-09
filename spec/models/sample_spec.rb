c0f7a5399f633ad1eb114cc9de057ff9ba9f6494
Gregory Tomei <gregtomei@gmail.com>
2008-07-09 16:24:19 -0700

    adding tests for the Sample, especially for its new page_uri field

--- spec/models/sample_spec.rb
+++ spec/models/sample_spec.rb
@@ -9,8 +9,30 @@ describe "Sample create" do
     @sample.should be_valid
   end
   
+  it "should respond to its attribute methods" do
+    @sample.should respond_to(:exercise)
+    @sample.should respond_to(:page)
+    @sample.should respond_to(:passed)
+    @sample.should respond_to(:user)
+    @sample.should respond_to(:response)
+    @sample.should respond_to(:time)
+    @sample.should respond_to(:page_data)
+    @sample.should respond_to(:page_size)
+    @sample.should respond_to(:memory)
+    @sample.should respond_to(:page_uri)
+    @sample.should respond_to(:created_at)
+    @sample.should respond_to(:updated_at)
+  end
+  
   it "should be savable" do
     @sample.save       .should equal(true)
     @sample.new_record?.should equal(false)
+  end
+  
+  it "should save the data given to it" do
+    sample_count    = Sample.count
+    sample_page_uri = "/uri/for/test/1/page"
+    @sample         = Sample.create(:page_uri => sample_page_uri)
+    @sample.reload.page_uri.should == sample_page_uri
   end
 end
