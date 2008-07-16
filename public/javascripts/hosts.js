e3416e4b21379f727d543a1250e8d929458f12e1
Gregory Tomei <gregtomei@gmail.com>
2008-07-16 14:59:33 -0700

    fixing typo for the jquery syntax and adding comment on this.

--- public/javascripts/hosts.js
+++ public/javascripts/hosts.js
@@ -9,7 +9,7 @@ GroundControl.Hosts.watch_exercise_init = function(exercise_div_id, exercise_id)
     if (exercise_id <= 0) return exercise_id; // allow a pass thru for testing/mocking
     
     var refreshId = setInterval(function() {
-         $(exercise_div_id).load('/hosts/exercise/' + exercise_id);
+         $('#' + exercise_div_id).load('/hosts/exercise/' + exercise_id); // this line uses jquery
     }, 3000);
     return refreshId;
 }
