96d9b7b7d43eefef423eb615aa3ee4ec41865971
Gregory Tomei <gregtomei@gmail.com>
2008-07-24 15:07:17 -0700

    adding some tests for the Hosts Controller

---
+++ spec/controllers/hosts_controller_spec.rb
@@ -0,0 +1,313 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe HostsController do
+  describe "handling GET /hosts" do
+
+    before(:each) do
+      @host = mock_model(Host)
+      Host.stub!(:find).and_return([@host])
+    end
+  
+    def do_get
+      get :index
+    end
+  
+    it "should be successful" do
+      do_get
+      response.should be_success
+    end
+
+    it "should render index template" do
+      do_get
+      response.should render_template('index')
+    end
+  
+    it "should find all hosts" do
+      Host.should_receive(:find).with(:all).and_return([@host])
+      do_get
+    end
+  
+    it "should assign the found hosts for the view" do
+      do_get
+      assigns[:hosts].should == [@host]
+    end
+  end
+
+  describe "handling GET /hosts.xml" do
+
+    before(:each) do
+      @host = mock_model(Host, :to_xml => "XML")
+      Host.stub!(:find).and_return(@host)
+    end
+  
+    def do_get
+      @request.env["HTTP_ACCEPT"] = "application/xml"
+      get :index
+    end
+  
+    it "should be successful" do
+      do_get
+      response.should be_success
+    end
+
+    it "should find all hosts" do
+      Host.should_receive(:find).with(:all).and_return([@host])
+      do_get
+    end
+  
+    it "should render the found hosts as xml" do
+      @host.should_receive(:to_xml).and_return("XML")
+      do_get
+      response.body.should == "XML"
+    end
+  end
+
+  describe "handling GET /hosts/1" do
+
+    before(:each) do
+      @host = mock_model(Host)
+      Host.stub!(:find).and_return(@host)
+    end
+  
+    def do_get
+      get :show, :id => "1"
+    end
+
+    it "should be successful" do
+      do_get
+      response.should be_success
+    end
+  
+    it "should render show template" do
+      do_get
+      response.should render_template('show')
+    end
+  
+    it "should find the host requested" do
+      Host.should_receive(:find).with("1").and_return(@host)
+      do_get
+    end
+  
+    it "should assign the found host for the view" do
+      do_get
+      assigns[:host].should equal(@host)
+    end
+  end
+
+  describe "handling GET /hosts/1.xml" do
+
+    before(:each) do
+      @host = mock_model(Host, :to_xml => "XML")
+      Host.stub!(:find).and_return(@host)
+    end
+  
+    def do_get
+      @request.env["HTTP_ACCEPT"] = "application/xml"
+      get :show, :id => "1"
+    end
+
+    it "should be successful" do
+      do_get
+      response.should be_success
+    end
+  
+    it "should find the host requested" do
+      Host.should_receive(:find).with("1").and_return(@host)
+      do_get
+    end
+  
+    it "should render the found host as xml" do
+      @host.should_receive(:to_xml).and_return("XML")
+      do_get
+      response.body.should == "XML"
+    end
+  end
+
+  describe "handling GET /hosts/new" do
+
+    before(:each) do
+      @host = mock_model(Host)
+      Host.stub!(:new).and_return(@host)
+    end
+  
+    def do_get
+      get :new
+    end
+
+    it "should be successful" do
+      do_get
+      response.should be_success
+    end
+  
+    it "should render new template" do
+      do_get
+      response.should render_template('new')
+    end
+  
+    it "should create an new host" do
+      Host.should_receive(:new).and_return(@host)
+      do_get
+    end
+  
+    it "should not save the new host" do
+      @host.should_not_receive(:save)
+      do_get
+    end
+  
+    it "should assign the new host for the view" do
+      do_get
+      assigns[:host].should equal(@host)
+    end
+  end
+
+  describe "handling GET /hosts/1/edit" do
+
+    before(:each) do
+      @host = mock_model(Host)
+      Host.stub!(:find).and_return(@host)
+    end
+  
+    def do_get
+      get :edit, :id => "1"
+    end
+
+    it "should be successful" do
+      do_get
+      response.should be_success
+    end
+  
+    it "should render edit template" do
+      do_get
+      response.should render_template('edit')
+    end
+  
+    it "should find the host requested" do
+      Host.should_receive(:find).and_return(@host)
+      do_get
+    end
+  
+    it "should assign the found Host for the view" do
+      do_get
+      assigns[:host].should equal(@host)
+    end
+  end
+
+  describe "handling POST /hosts" do
+
+    before(:each) do
+      @host = mock_model(Host, :to_param => "1")
+      Host.stub!(:new).and_return(@host)
+    end
+    
+    describe "with successful save" do
+  
+      def do_post
+        @host.should_receive(:save).and_return(true)
+        post :create, :host => {}
+      end
+  
+      it "should create a new host" do
+        Host.should_receive(:new).with({}).and_return(@host)
+        do_post
+      end
+
+      it "should redirect to the new host" do
+        do_post
+        response.should redirect_to(host_url("1"))
+      end
+      
+    end
+    
+    describe "with failed save" do
+
+      def do_post
+        @host.should_receive(:save).and_return(false)
+        post :create, :host => {}
+      end
+  
+      it "should re-render 'new'" do
+        do_post
+        response.should render_template('new')
+      end
+      
+    end
+  end
+
+  describe "handling PUT /hosts/1" do
+
+    before(:each) do
+      @host = mock_model(Host, :to_param => "1")
+      Host.stub!(:find).and_return(@host)
+    end
+    
+    describe "with successful update" do
+
+      def do_put
+        @host.should_receive(:update_attributes).and_return(true)
+        put :update, :id => "1"
+      end
+
+      it "should find the host requested" do
+        Host.should_receive(:find).with("1").and_return(@host)
+        do_put
+      end
+
+      it "should update the found host" do
+        do_put
+        assigns(:host).should equal(@host)
+      end
+
+      it "should assign the found host for the view" do
+        do_put
+        assigns(:host).should equal(@host)
+      end
+
+      it "should redirect to the host" do
+        do_put
+        response.should redirect_to(host_url("1"))
+      end
+
+    end
+    
+    describe "with failed update" do
+
+      def do_put
+        @host.should_receive(:update_attributes).and_return(false)
+        put :update, :id => "1"
+      end
+
+      it "should re-render 'edit'" do
+        do_put
+        response.should render_template('edit')
+      end
+
+    end
+  end
+
+  describe "handling DELETE /hosts/1" do
+
+    before(:each) do
+      @host = mock_model(Host, :destroy => true)
+      Host.stub!(:find).and_return(@host)
+    end
+  
+    def do_delete
+      delete :destroy, :id => "1"
+    end
+
+    it "should find the host requested" do
+      Host.should_receive(:find).with("1").and_return(@host)
+      do_delete
+    end
+  
+    it "should call destroy on the found host" do
+      @host.should_receive(:destroy)
+      do_delete
+    end
+  
+    it "should redirect to the hosts list" do
+      do_delete
+      response.should redirect_to(hosts_url)
+    end
+  end
+end
---
+++ spec/controllers/hosts_routing_spec.rb
@@ -0,0 +1,61 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe HostsController do
+  describe "route generation" do
+
+    it "should map { :controller => 'hosts', :action => 'index' } to /hosts" do
+      route_for(:controller => "hosts", :action => "index").should == "/hosts"
+    end
+  
+    it "should map { :controller => 'hosts', :action => 'new' } to /hosts/new" do
+      route_for(:controller => "hosts", :action => "new").should == "/hosts/new"
+    end
+  
+    it "should map { :controller => 'hosts', :action => 'show', :id => 1 } to /hosts/1" do
+      route_for(:controller => "hosts", :action => "show", :id => 1).should == "/hosts/1"
+    end
+  
+    it "should map { :controller => 'hosts', :action => 'edit', :id => 1 } to /hosts/1/edit" do
+      route_for(:controller => "hosts", :action => "edit", :id => 1).should == "/hosts/1/edit"
+    end
+  
+    it "should map { :controller => 'hosts', :action => 'update', :id => 1} to /hosts/1" do
+      route_for(:controller => "hosts", :action => "update", :id => 1).should == "/hosts/1"
+    end
+  
+    it "should map { :controller => 'hosts', :action => 'destroy', :id => 1} to /hosts/1" do
+      route_for(:controller => "hosts", :action => "destroy", :id => 1).should == "/hosts/1"
+    end
+  end
+
+  describe "route recognition" do
+
+    it "should generate params { :controller => 'hosts', action => 'index' } from GET /hosts" do
+      params_from(:get, "/hosts").should == {:controller => "hosts", :action => "index"}
+    end
+  
+    it "should generate params { :controller => 'hosts', action => 'new' } from GET /hosts/new" do
+      params_from(:get, "/hosts/new").should == {:controller => "hosts", :action => "new"}
+    end
+  
+    it "should generate params { :controller => 'hosts', action => 'create' } from POST /hosts" do
+      params_from(:post, "/hosts").should == {:controller => "hosts", :action => "create"}
+    end
+  
+    it "should generate params { :controller => 'hosts', action => 'show', id => '1' } from GET /hosts/1" do
+      params_from(:get, "/hosts/1").should == {:controller => "hosts", :action => "show", :id => "1"}
+    end
+  
+    it "should generate params { :controller => 'hosts', action => 'edit', id => '1' } from GET /hosts/1;edit" do
+      params_from(:get, "/hosts/1/edit").should == {:controller => "hosts", :action => "edit", :id => "1"}
+    end
+  
+    it "should generate params { :controller => 'hosts', action => 'update', id => '1' } from PUT /hosts/1" do
+      params_from(:put, "/hosts/1").should == {:controller => "hosts", :action => "update", :id => "1"}
+    end
+  
+    it "should generate params { :controller => 'hosts', action => 'destroy', id => '1' } from DELETE /hosts/1" do
+      params_from(:delete, "/hosts/1").should == {:controller => "hosts", :action => "destroy", :id => "1"}
+    end
+  end
+end
