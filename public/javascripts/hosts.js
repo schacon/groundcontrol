52599e1a269b438701af20d85e2301ed63745e1f
Gregory Tomei <gregtomei@gmail.com>
2008-08-12 16:59:56 -0700

    adding JS to collect the AUT version and note info from the end-user and submit them to the run_exercise_* action. the action still needs to be modified to receive these data

--- public/javascripts/hosts.js
+++ public/javascripts/hosts.js
@@ -3,8 +3,8 @@ if (typeof GroundControl == "undefined"){ var GroundControl = {}; }
 
 // create the first class
 GroundControl.Hosts = {};
-// initialize the page refreshing for the watch_exercise view
 
+// initialize the page refreshing for the watch_exercise view
 GroundControl.Hosts.watch_exercise_init = function(exercise_div_id, exercise_id) {
     if (exercise_id <= 0) return exercise_id; // allow a pass thru for testing/mocking
     
@@ -12,4 +12,18 @@ GroundControl.Hosts.watch_exercise_init = function(exercise_div_id, exercise_id)
          $('#' + exercise_div_id).load('/hosts/exercise/' + exercise_id); // this line uses jquery
     }, 3000);
     return refreshId;
+}
+
+GroundControl.Hosts.run_performance_test = function(submit_to_url) {
+    var aut_version = prompt("Enter build number (optional)");
+    var aut_note    = prompt("Enter any notes you care to record for this test (optional)");
+    
+    // makes use of the Prototype JS lib
+    var form_el                = new Element('form',  {'method': 'POST',  'action': submit_to_url});
+    var form_field_aut_version = new Element('input', {  'type': 'hidden', 'value': aut_version});
+    var form_field_aut_note    = new Element('input', {  'type': 'hidden', 'value': aut_note});
+    Element.insert(document.body, form_el);
+    Element.insert(form_el, form_field_aut_version);
+    Element.insert(form_el, form_field_aut_note);
+    form_el.submit();
 }
