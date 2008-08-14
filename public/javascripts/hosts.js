8a9a43c81eea08de8c929feac046e1e7a5d413c3
Gregory Tomei <gregtomei@gmail.com>
2008-08-14 14:45:09 -0700

    removing unused code

--- public/javascripts/hosts.js
+++ public/javascripts/hosts.js
@@ -14,22 +14,6 @@ GroundControl.Hosts.watch_exercise_init = function(exercise_div_id, exercise_id)
     return refreshId;
 }
 
-// initiates a performance test, gathers AUT info from the user.
-// aut_version_field_name, aut_note_field_name are passed as args to avoid storing model field names here in JS.
-GroundControl.Hosts.exercise_performance = function(submit_to_url, aut_version_field_name, aut_note_field_name) {
-    var is_test     = (arguments[3] == true); // if passed, ths 4th arg indicates that this method is called as part of an automated test
-    var aut_version = is_test ? '': prompt("Enter build number (optional)");
-    var aut_note    = is_test ? '': prompt("Enter any notes you care to record for this test (optional)");
-    // makes use of the Prototype JS lib
-    var form_el                = new Element('form',  {'method':'POST',  'action':submit_to_url});
-    var form_field_aut_version = new Element('input', {  'type':'hidden',  'name':aut_version_field_name, 'value':aut_version});
-    var form_field_aut_note    = new Element('input', {  'type':'hidden',  'name':aut_note_field_name,    'value':aut_note});
-    Element.insert(document.body, form_el);
-    Element.insert(form_el,       form_field_aut_version);
-    Element.insert(form_el,       form_field_aut_note);
-    if (is_test) return form_el;
-    form_el.submit();
-}
 
 GroundControl.Hosts.show_exercise_form = function(element_id) {
     $(element_id).show();
