963bdad08a5a4f3b19c9d200d5c6ed3e0e9c0c68
Gregory Tomei <gregtomei@gmail.com>
2008-08-19 11:55:11 -0700

    adding test case for Stack.roles association

--- spec/models/stack_spec.rb
+++ spec/models/stack_spec.rb
@@ -1,11 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe Stack do
+describe "Empty Stack" do
   before(:each) do
     @stack = Stack.new
   end
 
-  it "should be valid" do
+  it "should be valid while empty" do
     @stack.should be_valid
   end
   
@@ -15,5 +15,23 @@ describe Stack do
   
   it "should have the expected associations" do
     @stack.roles.should be_empty
+  end
+end
+
+describe "Create Stack" do
+  before(:each) do
+    @stack = Stack.new
+  end
+  
+  it "should be able to be populated with data and saved" do
+    new_name = "Stack rspec test"
+    @stack.name == new_name
+    @stack.roles << roles(:rmc_internal)
+    @stack.roles << roles(:web_app1)
+    
+    @stack.should be_valid
+    @stack.save.should == true
+    @stack.roles.should_not be_empty
+    @stack.roles.size.should == 2
   end
 end
