0ec4b551767b67d445288efbed1f01637fd3495a
Gregory Tomei <gregtomei@gmail.com>
2008-08-19 10:01:49 -0700

    updating models schema annotations

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20080812205834
+# Schema version: 20080819160004
 #
 # Table name: pages
 #
--- app/models/stack.rb
+++ app/models/stack.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+# Schema version: 20080819160004
+#
+# Table name: stacks
+#
+#  id         :integer(11)     not null, primary key
+#  name       :string(50)      
+#  created_at :datetime        
+#  updated_at :datetime        
+#
+
 class Stack < ActiveRecord::Base
 end
