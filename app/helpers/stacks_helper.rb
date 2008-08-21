eb273a0e5b263d3a5e2c40972343a09642acdfa1
Gregory Tomei <gregtomei@gmail.com>
2008-08-21 16:33:35 -0700

    UI for specifying the Hosts for a Stack is working and with tests

--- app/helpers/stacks_helper.rb
+++ app/helpers/stacks_helper.rb
@@ -1,2 +1,6 @@
 module StacksHelper
+  def display_hosts(stack)
+    # KLUDGE: using the +link_to+ method here to generate the hyperlink breaks rspec tests
+    stack.hosts.collect{ |h| "<a href=\"#{host_path(h)}\">#{h.hostname}</a>" }.join(", ")
+  end
 end
